62efeceaac0b5a0a15ec913125bf0f9289c4ec66
Luis <bachejin@gmail.com>
2021-11-24 01:16:32 -0600

    Fixed exercise 1.1
    Added props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,39 +7,53 @@ const part2 = "Using props to pass data";
 const exercises2 = 7;
 const part3 = "State of a component";
 const exercises3 = 14;
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
-      <h1>{course}</h1>
+      <h1>{props.course}</h1>
     </div>
   );
 };
-const Content = () => {
+const Content = (props) => {
   return (
     <div>
       <p>
-        {part1} {exercises1}
+        {props.part1} {props.exercises1}
       </p>
       <p>
-        {part2} {exercises2}
+        {props.part2} {props.exercises2}
       </p>
       <p>
-        {part3} {exercises3}
+        {props.part3} {props.exercises3}
       </p>
     </div>
   );
 };
-const Total = () => (
+const Total = (props) => (
   <div>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>
+      Number of exercises{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
   </div>
 );
 const App = () => {
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
